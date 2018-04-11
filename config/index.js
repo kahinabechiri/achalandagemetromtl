202486c8309fe0d9af3abfd8d49493569bdcf61d
Kahina Bechiri <koslambrou@gmail.com>
2018-04-10 20:49:17 -0400

    Correction

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist/achalandagemetromtl'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/achalandagemetromtl',
 
     /**
      * Source Maps
